e17ff410f4e82a7f33d0e189d7d57f97668efe5e
ED <emaddeve20@gmail.com>
2021-04-16 09:52:00 +0430

    change dir name

--- setup.py
+++ setup.py
@@ -25,8 +25,8 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "gnu2048"},
-    packages=setuptools.find_packages(where="gnu2048"),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
 )
 
